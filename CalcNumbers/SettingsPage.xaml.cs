68961e011dea89393b4577503b37b4672bb66ba6
Egor Ivanov <egor.i.ivanov@gmail.com>
2024-06-23 10:50:18 +0200

    settings page

---
+++ CalcNumbers/SettingsPage.xaml.cs
@@ -0,0 +1,24 @@
+namespace CalcNumbers
+{
+    public partial class SettingsPage : ContentPage
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnSaveClicked(object sender, EventArgs e)
+        {
+            if (int.TryParse(MaxScoreEntry.Text, out int maxScore) && maxScore > 0)
+            {
+                Preferences.Set("MaxScore", maxScore);
+                await DisplayAlert("Settings", "Max score has been saved.", "OK");
+                await Navigation.PopAsync();
+            }
+            else
+            {
+                await DisplayAlert("Invalid Input", "Please enter a valid number greater than 0.", "OK");
+            }
+        }
+    }
+}
