a87cb6e70e9171bb777cfcb53903408735251606
Egor Ivanov <egor.i.ivanov@gmail.com>
2024-06-29 09:38:55 +0200

    fix: csproj versions

--- Update-Csproj.ps1
+++ Update-Csproj.ps1
@@ -4,13 +4,22 @@ param (
     [string]$VersionCode
 )
 
-Write-Host "VersionCode: $VersionCode"
-Write-Host "DisplayVersion: $DisplayVersion"
-
 [xml]$xml = Get-Content $CsprojPath
+
 $namespace = @{ "msbuild" = "http://schemas.microsoft.com/developer/msbuild/2003" }
 
-$xml.Project.PropertyGroup.ApplicationDisplayVersion = $DisplayVersion
-$xml.Project.PropertyGroup.ApplicationVersion = $VersionCode
+$applicationDisplayVersionNode = $xml.Project.PropertyGroup.ChildNodes | Where-Object { $_.LocalName -eq "ApplicationDisplayVersion" }
+if (-not $applicationDisplayVersionNode) {
+    $applicationDisplayVersionNode = $xml.CreateElement("ApplicationDisplayVersion", $namespace.msbuild)
+    $xml.Project.PropertyGroup.AppendChild($applicationDisplayVersionNode) | Out-Null
+}
+$applicationDisplayVersionNode.InnerText = $DisplayVersion
+
+$applicationVersionNode = $xml.Project.PropertyGroup.ChildNodes | Where-Object { $_.LocalName -eq "ApplicationVersion" }
+if (-not $applicationVersionNode) {
+    $applicationVersionNode = $xml.CreateElement("ApplicationVersion", $namespace.msbuild)
+    $xml.Project.PropertyGroup.AppendChild($applicationVersionNode) | Out-Null
+}
+$applicationVersionNode.InnerText = $VersionCode
 
 $xml.Save($CsprojPath)
